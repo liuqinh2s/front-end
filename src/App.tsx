6c1871754b9603f3bb85c60fea5b5339017fbe59
liuqinh2s <liuqinh2s@gmail.com>
2021-08-07 23:16:34 +0800

    第一次提交

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <header className="App-header">合约量化交易机器人</header>
+      <div className="App-body">
+        <form
+          className="register-form"
+          action="http://127.0.0.1:5000/register"
+          method="post"
         >
-          Learn React
-        </a>
-      </header>
+          <input placeholder="api-key" type="text" name="api-key"></input>
+          <input placeholder="secret-key" type="text" name="secret-key"></input>
+          <input placeholder="notify-uid" type="text" name="notify-uid"></input>
+          <input
+            placeholder="开仓数(多少个ETH)"
+            type="text"
+            name="quantity"
+          ></input>
+          <input placeholder="合约倍数" type="text" name="leverage"></input>
+          <input
+            placeholder="联系方式(电话或者邮箱)"
+            type="text"
+            name="contact"
+          ></input>
+          <input placeholder="用户名" type="text" name="username"></input>
+          <input type="submit" value="提交"></input>
+        </form>
+        <form
+          className="stop-form"
+          action="http://127.0.0.1:5000/stop"
+          method="post"
+        >
+          <input className="circle-button" type="submit" value="停机"></input>
+        </form>
+      </div>
     </div>
   );
 }
